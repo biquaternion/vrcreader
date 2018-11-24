6cbe2f1b1d7bf2851dd3b8fd1652e503204d9339
biquaternion <biquaternion@gmail.com>
2018-11-24 20:30:36 +0300

    added class Strobe, which encapsulates strobe routines

--- python/vrc_reader.py
+++ python/vrc_reader.py
@@ -9,44 +9,66 @@ import sys
 
 default_filename = '131206N797_1315_1195_01.vrc'
 default_fps = 25
-term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+
+
+class Strobe:
+    def __init__(self, left=0, top=0, width=0, height=0):
+        self.rect = (left, top, width, height)
+        self.ready = False
+        self.term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+
+    def modify(self, rect, make_ready=True):
+        if make_ready:
+            self.ready = True
+        self.rect = rect
+
+    def center(self):
+        return self.rect[0] + self.rect[2] // 2, self.rect[1] + self.rect[3] // 2
+
+    def calc_new_position(self, im):
+        ret, new_pos = cv2.meanShift(im[:, :, 0], self.rect, self.term_crit)
+        if ret:
+            # todo: check SNR inside strobe to move it
+            self.rect = new_pos
+
+    def draw(self, im):
+        if self.ready:
+            cv2.rectangle(im,
+                          (self.rect[0], self.rect[1]),
+                          (self.rect[0] + self.rect[2], self.rect[1] + self.rect[3]),
+                          (0, 255, 255), thickness=3)
+            cv2.rectangle(im,
+                          (self.rect[0], self.rect[1]),
+                          (self.rect[0] + self.rect[2], self.rect[1] + self.rect[3]),
+                          (255, 0, 0), thickness=1)
 
 
 def on_click(event, x, y, flags, param):
     strobe = param
     if event == cv2.EVENT_LBUTTONDOWN:
-        strobe[0] = x - 20
-        strobe[1] = y - 20
-        strobe[2] = 40
-        strobe[3] = 40
-
-
-def draw_strobe(im, strobe):
-    if strobe[2] == 0:  # todo: invent good check condition
-        return
-    cv2.rectangle(im,
-                  (strobe[0], strobe[1]),
-                  (strobe[0] + strobe[2], strobe[1] + strobe[3]),
-                  (0, 255, 255), thickness=3)
-    cv2.rectangle(im,
-                  (strobe[0], strobe[1]),
-                  (strobe[0] + strobe[2], strobe[1] + strobe[3]),
-                  (255, 0, 0),
-                  thickness=1)
+        print('new strobe position', x, y)
+        tb_pos = cv2.getTrackbarPos("strobe size", "im")
+        strobe.modify((x - tb_pos // 2, y - tb_pos // 2, tb_pos, tb_pos))
 
 
 def play_all(frames, frame_period=40, paused=False):
-    strobe = [0, 0, 0, 0]
+    strobe = Strobe(0, 0, 0, 0)
     cv2.namedWindow('im')
+
+    def on_trackbar(param):
+        tb_pos = param
+        x, y = strobe.center()
+        strobe.modify((x - tb_pos // 2, y - tb_pos // 2, tb_pos, tb_pos), False)
+
+    cv2.createTrackbar('strobe size', 'im', 40, 255, on_trackbar)
     cv2.setMouseCallback('im', on_click, strobe)
     for i in range(frames):
         fr = np.copy(frame[i])
         img = cv2.merge(np.array([fr, fr, fr]))
         # img = cv2.equalizeHist(img)
-        if strobe[2] != 0:
-            ret, s = cv2.meanShift(img[:, :, 0], (strobe[0], strobe[1], strobe[2], strobe[3]), term_crit)
-            strobe = [s[0], s[1], s[2], s[3]]
-        draw_strobe(img, strobe)
+        if strobe.ready:
+            strobe.calc_new_position(img)
+            strobe.draw(img)
         cv2.imshow('im', img)
         if paused:
             period = 0
